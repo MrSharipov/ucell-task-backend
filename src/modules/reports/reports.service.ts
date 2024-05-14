27b9d31ed0650277f6fa74db9fa7476164a7e0fd
MrSharipov <84614549+MrSharipov@users.noreply.github.com>
2024-05-14 13:27:25 +0500

    row query has been added

--- src/modules/reports/reports.service.ts
+++ src/modules/reports/reports.service.ts
@@ -18,6 +18,64 @@ export class ReportsService {
     this.model = prisma.reports;
   }
 
+  async getGroupByRegion() {
+    /*
+          The first way: With row SQL
+    */
+
+    const regionSums = await this.prisma.$queryRaw`
+      SELECT region, CAST(SUM(subscribers) AS INTEGER) as totalSubscribers
+      FROM "Reports"
+      GROUP BY "region"
+    `;
+
+    const uzbekistanTotal = await this.prisma.$queryRaw`
+      SELECT CAST(SUM(subscribers) AS INTEGER)
+      FROM "Reports"
+    `;
+
+    /*
+        The second way: With findMany (not efficient)
+  */
+
+    // const reports = await this.prisma.reports.findMany();
+    //
+    // // Initialize an array to store region sums
+    // const regionSums = [];
+    //
+    // // Calculate sums for each region
+    // reports.forEach(report => {
+    //   const regionIndex = regionSums.findIndex(item => item.region === report.region);
+    //   if (regionIndex !== -1) {
+    //     // Update sum if region already exists in the array
+    //     regionSums[regionIndex].subscribers += report.subscribers;
+    //   } else {
+    //     // Add new region to the array
+    //     regionSums.push({ region: report.region, subscribers: report.subscribers });
+    //   }
+    // });
+    //
+    // // Calculate total subscribers for Uzbekistan
+    // const uzbekistanTotal = reports.reduce((acc, report) => {
+    //   acc = acc + report.subscribers;
+    //   return acc;
+    // }, 0);
+
+    return { regionSums, uzbekistanTotal: uzbekistanTotal[0].sum };
+  }
+
+  async create(data: ReportsCreateDto): Promise<IAnyObject> {
+    try {
+      const result = await this.model.create({
+        data,
+      });
+
+      return appGenerateResponse({ id: result.id });
+    } catch (e) {
+      throw e;
+    }
+  }
+
   // async getListByPagination(query: ReportsListQueryDto): Promise<any> {
   //   const filter: Prisma.ReportsWhereInput = this.generateFilterByQuery(query);
   //   const totalCount = await this.model.count({
@@ -50,43 +108,4 @@ export class ReportsService {
   //   );
   //   return where;
   // }
-
-  async getGroupByRegion() {
-    const reports = await this.prisma.reports.findMany();
-
-    // Initialize an array to store region sums
-    const regionSums = [];
-
-    // Calculate sums for each region
-    reports.forEach(report => {
-      const regionIndex = regionSums.findIndex(item => item.region === report.region);
-      if (regionIndex !== -1) {
-        // Update sum if region already exists in the array
-        regionSums[regionIndex].subscribers += report.subscribers;
-      } else {
-        // Add new region to the array
-        regionSums.push({ region: report.region, subscribers: report.subscribers });
-      }
-    });
-
-    // Calculate total subscribers for Uzbekistan
-    const uzbekistanTotal = reports.reduce((acc, report) => {
-      acc = acc + report.subscribers;
-      return acc;
-    }, 0);
-
-    return { regionSums, uzbekistanTotal };
-  }
-
-  async create(data: ReportsCreateDto): Promise<IAnyObject> {
-    try {
-      const result = await this.model.create({
-        data,
-      });
-
-      return appGenerateResponse({ id: result.id });
-    } catch (e) {
-      throw e;
-    }
-  }
 }
